75ebf315ab4395adbd1fdbbadcfbf5948dd7ac2a
akelsaman <ak.elsaman@gmail.com>
2015-12-27 22:30:16 +0200

    201512272230

--- hikma/anticounterfeit/static/UniqueRandomNumbers/add.js
+++ hikma/anticounterfeit/static/UniqueRandomNumbers/add.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("select#UniqueRandomNumber-Product").load("/anticounterfeit/product");
+    $("select#URN-product").load("/anticounterfeit/product");
 	$("input#URN-add").click(addClick);
 	/*validatorCodeStart*/
 	validatorRun();
@@ -7,6 +7,16 @@ $(document).ready(function(){
 });
 function addClick() {
     postArray = {}
-    $("[name]").each(function(){postArray[$(this).attr("name")] = $(this).val();});
-    alert(postArray["URN-uniqueRandomNumbersCount"]);
+    //$("[name]").each(function(){postArray[$(this).attr("name")] = $(this).val();});
+    $("[name]").each(function(){
+        if($(this).attr("type")=="radio"){
+            if($(this).is(":checked")){
+                postArray[$(this).attr("name")] = $(this).val();
+            }
+        } else {
+            postArray[$(this).attr("name")] = $(this).val();
+        }
+    });
+    alert(postArray["URN-internalOrExternal"]);
+
 }
